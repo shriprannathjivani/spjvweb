63173a86f1b896fd6b39449613be7749e4c0d01f
ashish0038 <ashish.0038@gmail.com>
2026-02-24 20:28:18 +0530

    Updates

---
+++ _next/static/15iP2My2vPFJsVs6UQr_-/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fgyanbhandar\u002Farticle\u002F[slug]","\u002Fgyanbhandar\u002Fbook\u002F[slug]","\u002Fgyanbhandar\u002Fquiz\u002F[slug]","\u002Fsatguru\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
