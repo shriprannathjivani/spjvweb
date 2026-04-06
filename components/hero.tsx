b4eccc7f5a124538b0050ff1ee71b91f842da814
ashish0038 <ashish.0038@gmail.com>
2026-04-06 15:25:12 +0530

    added images

--- components/hero.tsx
+++ components/hero.tsx
@@ -15,11 +15,30 @@ import { CarouselDots } from "@/components/carousel-dots";
 import { useEffect, useRef, useState } from "react";
 import { TextAnimate } from "@/components/ui/text-animate"
 import { motion } from "motion/react";
+import { Link } from "lucide-react";
+import AutoHeight from "embla-carousel-auto-height";
 
 const slides = [
   {
+    title: "श्री प्राणनाथ जी' स्वरूप",
+    highlight: "ज्ञान की दोपहरी का वह सूरज",
+    link: "",
+    description:
+      `श्री प्राणनाथ जी का स्वरूप ज्ञान की दोपहरी का वह सूरज है, जिसके उग जाने पर अध्यात्म जगत में किसी भी प्रकार का अन्धकार रूपी संशय नहीं रहता। वेदों की ऋचायें जिस को खोजती हैं, दर्शन ग्रन्थ जिस सत्य को पाना चाहते हैं, गीता और भागवत जिस परम लक्ष्य उत्तम पुरुष की ओर संकेत करते हैं, कुरान की आयतें जिस अल्लाह तआला का वर्णन करना चाहती हैं, बाइबल जिस प्रेम के स्वरूप का वर्णन करने का प्रयास करती है और सन्तों की वाणियाँ जिस सत्य की ओर संकेत करती हैं, उसकी पूर्ण प्राप्ति श्री प्राणनाथ जी की वाणी में निहित है।`,
+    image: "/shriprannathji.png",
+  },
+  {
+    title: "श्री राजन स्वामी",
+    highlight: "||ब्रह्मज्ञान ही अमृत है - प्रेम ही जीवन है||",
+    link: "https://www.spjin.org/",
+    description:
+      `ज्ञान, शिक्षा, उच्च आदर्श, पावन चरित्र, व श्री प्राणनाथ जी की ब्रह्मवाणी (निजानन्द दर्शन) के साथ साथ भारतीय संस्कृति का समाज में प्रचार करना, तथा वैज्ञानिक सिद्धान्तों पर आधारित आध्यात्मिक मूल्यों द्वारा मानव को महामानव बनाना । - \n श्री प्राणनाथ ज्ञानपीठ`,
+    image: "/swamiji_slider.png",
+  },
+  {
     title: "जागिए। उठिए। ",
     highlight: "पहचानिए अपना सत्य।",
+    link: "",
     description: `श्री प्राणनाथ जी द्वारा आवेशित तारतम वाणी आत्मा को उसके शाश्वत प्रियतम से जोड़ती है और सभी धर्मों के मूल सत्य को एक सूत्र में पिरोती है। हमारे सतगुरु तारतम वाणी से असंख्य हृदयों को जागृत कर रहे हैं। उनके चरणों का अनुसरण करते हुए, 'श्री प्राणनाथ जी वाणी' SPJV से जुड़े सभी सुंदरसाथ जी ज़ूम मीटिंग्स, यूट्यूब चैनल्स और सोशल मीडिया के माध्यम से वाणी की सुगंध का प्रसार कर रहे हैं और सबको प्रेम, सेवा और आत्म-जागृति के मार्ग पर चलने के लिए प्रेरित कर रहे हैं।
     `,
     image: "/awakesundarsath.png",
@@ -27,6 +46,7 @@ const slides = [
   {
     title: "बाल व युवा",
     highlight: "आत्मदर्शनम्",
+    link: "/balkendra",
     description:
       `बाल्यावस्था में हृदय कोमल और भावुक होता है और इस अवस्था में हृदय में अध्यात्म के पुष्प सहजता से प्रस्फुटित हो सकते हैं। श्री प्राणनाथ जी वाणी बाल एवं युवा आत्मदर्शनम् इसी उद्देश्य से रोचक मध्यमों से बाल/युवा वर्ग को धनी की वाणी से जोड़ने का प्रयास कर रहा है।`,
     image: "/Adsliderimg3.png",
@@ -38,7 +58,7 @@ export default function Hero() {
   const [api, setApi] = useState<CarouselApi>();
   const autoplay = useRef(
     Autoplay({
-      delay: 6000,
+      delay: 12000,
       stopOnInteraction: false,
     })
   );
@@ -67,11 +87,8 @@ export default function Hero() {
           <CarouselContent>
             {slides.map((slide, index) => (
               <CarouselItem key={index}>
-
-
                 {/* DESKTOP VERSION */}
                 <div className="hidden lg:grid lg:grid-cols-2 gap-14 items-center">
-
                   {/* LEFT */}
                   <div>
                     <h1 className="text-5xl font-bold leading-tight">
@@ -84,35 +101,32 @@ export default function Hero() {
                         </TextAnimate>
                       </span>
                     </h1>
-                    <TextAnimate animation="fadeIn" by="line" startOnView delay={0.3} className="mt-6 text-gray-700 text-lg max-w-xl">
+                    <TextAnimate animation="fadeIn" by="line" startOnView delay={0.3} className="mt-6 text-gray-700 text-lg max-w-xl ">
                       {slide.description}
                     </TextAnimate>
-                    <motion.div
-                      key={activeIndex === index ? "active" : "inactive"}
-                      initial={{ y: 60, opacity: 0 }}
-                      animate={{ y: 0, opacity: 1 }}
-                      transition={{
-                        duration: 0.6,
-                        delay: 0.3
-                      }}
-                      className="space-y-4"
-                    >
-                      <div className="mt-8 flex gap-4">
-                        <Button
-                          variant="outline"
-                          className="rounded-full border-2 border-black px-6 py-5 text-sm font-medium text-white bg-orange-500 hover:bg-orange-600 hover:text-white cursor-pointer"
-                        >
-                          प्रवचन देखें
-                        </Button>
-                        <Button
-                          variant="outline"
-                          className="rounded-full border-2 border-black px-6 py-5 text-sm font-medium hover:bg-black hover:text-white cursor-pointer"
-                        >
-                          जीवनी पढ़ें
-                        </Button>
-                      </div>
-                    </motion.div>
-
+                    {slide.link && (
+                      <motion.div
+                        key={activeIndex === index ? "active" : "inactive"}
+                        initial={{ y: 60, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                          duration: 0.6,
+                          delay: 0.3
+                        }}
+                        className="space-y-4"
+                      >
+                        <div className="mt-8 flex gap-4">
+                          <a href={slide.link}>
+                            <Button
+                              variant="outline"
+                              className="rounded-full border-2 border-black px-6 py-5 text-sm font-medium text-white bg-orange-500 hover:bg-orange-600 hover:text-white cursor-pointer"
+                            >
+                              अधिक जानिए
+                            </Button>
+                          </a>
+                        </div>
+                      </motion.div>
+                    )}
                   </div>
 
                   {/* RIGHT */}
@@ -149,7 +163,7 @@ export default function Hero() {
                   />
 
                   {/* Dark Gradient Overlay */}
-                  < div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent" />
+                  < div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent" />
 
                   {/* Text Content */}
                   < div className="absolute bottom-0 p-6 text-white" >
@@ -166,31 +180,29 @@ export default function Hero() {
                     <TextAnimate animation="fadeIn" by="line" startOnView delay={1} className="mt-3 text-sm text-gray-200 line-clamp-3">
                       {slide.description}
                     </TextAnimate>
-                    <motion.div
-                      key={activeIndex === index ? "active" : "inactive"}
-                      initial={{ y: 60, opacity: 0 }}
-                      animate={{ y: 0, opacity: 1 }}
-                      transition={{
-                        duration: 0.6,
-                        delay: 1.5
-                      }}
-                      className="space-y-4"
-                    >
-                      <div className="mt-8 flex gap-4">
-                        <Button
-                          variant="outline"
-                          className="rounded-full border-2 border-orange px-6 py-5 text-sm font-medium text-white bg-orange-500 hover:bg-orange-600 hover:text-white cursor-pointer"
-                        >
-                          प्रवचन देखें
-                        </Button>
-                        <Button
-                          variant="outline"
-                          className="rounded-full border-2 border-orange text-black px-6 py-5 text-sm font-medium hover:bg-black hover:text-white cursor-pointer"
-                        >
-                          जीवनी पढ़ें
-                        </Button>
-                      </div>
-                    </motion.div>
+                    {slide.link && (
+                      <motion.div
+                        key={activeIndex === index ? "active" : "inactive"}
+                        initial={{ y: 60, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                          duration: 0.6,
+                          delay: 0.3
+                        }}
+                        className="space-y-4"
+                      >
+                        <div className="mt-8 flex gap-4">
+                          <a href={slide.link}>
+                            <Button
+                              variant="outline"
+                              className="rounded-full border-2 border-black px-6 py-5 text-sm font-medium text-white bg-orange-500 hover:bg-orange-600 hover:text-white cursor-pointer"
+                            >
+                              अधिक जानिए
+                            </Button>
+                          </a>
+                        </div>
+                      </motion.div>
+                    )}
                   </div>
                 </div>
 
