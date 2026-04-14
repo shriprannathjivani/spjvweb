75e12460a12696b227a457223fefd97fa9bca03d
ashish0038 <ashish.0038@gmail.com>
2026-04-14 16:46:50 +0530

    added css and all

--- components/BodyWrapper.tsx
+++ components/BodyWrapper.tsx
@@ -28,7 +28,7 @@ export default function BodyWrapper({
     "karyakram-bg": pathname.startsWith("/karyakram"),
     "balkendra-bg": pathname.startsWith("/balkendra"),
     "temples-bg": pathname.startsWith("/temples"),
-    "no-HF": pathname.startsWith("/gamesnquiz"),
+    "no-HF": pathname.includes("/gamesnquiz"),
     "inner-bg":
       pathname !== "/" &&
       !pathname.startsWith("/satguru") &&
